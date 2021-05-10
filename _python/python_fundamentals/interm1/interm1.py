729d79118beff79e2e44cf767314a0ee88755dd6
wadeeawwad <wadee_awwad@hotmail.com>
2021-05-10 20:43:41 +0300

    intermediat1

---
+++ _python/python_fundamentals/interm1/interm1.py
@@ -0,0 +1,14 @@
+import random
+def randInt(min=0   , max=100  ):
+    num = round(random.random()*(max-min)+min)
+    num=0
+    if min <=max and max>0:
+        num=round(random.random()*(max-min)+min)
+    return num
+
+
+
+print(randInt())
+print(randInt(max=50))
+print(randInt(min=50))
+print(randInt(min=50, max=500))
