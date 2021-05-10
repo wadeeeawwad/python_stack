dfe7757696144f90ecb312ee5b5e59850d2dd64a
wadeeawwad <wadee_awwad@hotmail.com>
2021-05-10 20:55:39 +0300

    python_fundemantals

---
+++ _python/python_fundamentals/Functions Intermediate II/Functions Intermediate II.py
@@ -0,0 +1,53 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+#Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+x[1][0]=15
+print(x)
+#Change the last_name of the first student from 'Jordan' to 'Bryant'
+students[0]['last_name']='bryant'
+print(students)
+#In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'][0]='andres'
+#Change the value 20 in z to 30
+z[0]['y']=30
+print(z)
+
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(students):
+    for student in students:
+        for key,value in student.items():
+            print(f"{key}-{value}",end=", ")
+        print("")
+ 
+iterateDictionary(students)
+
+def iterateDictionary2(key_name,other_list):
+    for i in other_list:
+       
+        print(i[key_name])
+    
+ 
+iterateDictionary2('first_name',students)
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+#first_name - Michael, last_name - Jordan
+#first_name - John, last_name - Rosales
+#first_name - Mark, last_name - Guillen
+#first_name - KB, last_name - Tonel
